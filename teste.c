3925c0836e82fb225ab7618076ffe0b419f296bd
Andre <andrebasalo@gmail.com>
2017-09-09 15:17:09 -0300

    add função subtrair

--- teste.c
+++ teste.c
@@ -7,7 +7,8 @@ int main (void)
     int numero1 =0;
     int numero2 =0;
     int soma=0;
-
+    int menos=0;
+		
     printf("Digite o numero 1");
     scanf ("%d",&numero1);
     printf("Digite o numero 2");
@@ -15,7 +16,9 @@ int main (void)
 
     soma = numero1+numero2;
     printf("%d\n",soma);
-
+	
+    menos = nemero1 - numero2;
+    printf("%d",menos);
 
 
 
