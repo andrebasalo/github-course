deb58cc01f3645be0b8a4bde82552ce9566388c4
Andre prado <Andre@MacBook-Pro-de-Andre.local>
2017-09-09 14:46:37 -0300

    codigo teste em c

---
+++ teste.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main (void)
+{
+    int numero1 =0;
+    int numero2 =0;
+    int soma=0;
+
+    printf("Digite o numero 1");
+    scanf ("%d",&numero1);
+    printf("Digite o numero 2");
+    scanf ("%d",&numero2);
+
+    soma = numero1+numero2;
+    printf("%d\n",soma);
+
+
+
+
+}
